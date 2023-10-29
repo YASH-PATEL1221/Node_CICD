530c3af82c91f8f53d973e26721265c30a89aad4
YASH-PATEL1221 <100681742+YASH-PATEL1221@users.noreply.github.com>
2023-10-29 23:01:22 +0530

    should change in github

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! thiis is change');
+  res.send('Hello, World! this is change');
 });
 
 
