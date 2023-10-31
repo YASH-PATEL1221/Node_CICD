b70f76bb812f28bde5b5e0608795f39666adf30b
YASH-PATEL1221 <100681742+YASH-PATEL1221@users.noreply.github.com>
2023-10-31 12:02:27 +0530

    changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! this is change');
+});
+
+app.get('/home', (req, res) => {
+  console.log('agsd');
+  res.send('this is another change asd');
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
