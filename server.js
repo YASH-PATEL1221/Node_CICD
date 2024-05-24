012c4abbe2e9c1ba5f1358b56f63315e7368b157
yash patel <pyash2819@gmail.com>
2024-05-24 11:30:22 -0400

    made changes in port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/home', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
