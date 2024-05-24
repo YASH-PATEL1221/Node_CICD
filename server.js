abd8139351c5ec2882d2c6aed546e0a5e14eba3a
yash patel <pyash2819@gmail.com>
2024-05-23 21:29:29 -0400

    change port in node app

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/home', (req, res) => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
